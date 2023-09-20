42b0c12f48f569def98c9e0361909b591abdf090
NAJD ALEID <najdaleid6@gmail.com>
2023-09-20 09:22:29 -0700

    updated

--- printf_width.c
+++ printf_width.c
@@ -1,37 +1,64 @@
 #include "main.h"
-#include <stdbool.h>
 
 /**
- * get_width - Calculates the width for printing
+ * get_width_and_precision - Calculates width and precision for printing
  * @format: Formatted string in which to print the arguments.
  * @i: Pointer to the current position in the format string.
  * @list: List of arguments.
+ * @width: Output parameter to store the width.
+ * @precision: Output parameter to store the precision.
  *
- * Return: Width.
+ * Return: void (width and precision are returned through output parameters).
  */
-int get_width(const char *format, int *i, va_list list)
+void get_width_and_precision(const char *format, int *i, va_list list, int *width, int *precision)
 {
     int curr_i = *i;
-    int width = 0;
-    bool hasWidth = false;
+    *width = 0;
+    *precision = -1; 
 
-    if (format[curr_i] == '*')
+    
+    for (; format[curr_i] != '\0'; curr_i++)
+    {
+        if (is_digit(format[curr_i]))
+        {
+            *width *= 10;
+            *width += format[curr_i] - '0';
+        }
+        else if (format[curr_i] == '*')
+        {
+            curr_i++;
+            *width = va_arg(list, int);
+            break;
+        }
+        else
+            break;
+    }
+
+    
+    if (format[curr_i] == '.')
     {
         curr_i++;
-        width = va_arg(list, int);
-        hasWidth = true;
-    }
-    else
-    {
-        while (isdigit(format[curr_i]))
+        *precision = 0; 
+
+        /* Parse precision value*/
+        for (; format[curr_i] != '\0'; curr_i++)
         {
-            width = width * 10 + (format[curr_i] - '0');
-            curr_i++;
-            hasWidth = true;
+            if (is_digit(format[curr_i]))
+            {
+                *precision *= 10;
+                *precision += format[curr_i] - '0';
+            }
+            else if (format[curr_i] == '*')
+            {
+                curr_i++;
+                *precision = va_arg(list, int);
+                break;
+            }
+            else
+                break;
         }
     }
 
     *i = curr_i - 1;
-
-    return hasWidth ? width : -1; /* Return -1 if no width was specified */
 }
+
